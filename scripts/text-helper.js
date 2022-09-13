7d81fc270d4010b73ea39491571b06e282652dd7
Racooder <76227605+Racooder@users.noreply.github.com>
2022-09-13 19:38:37 +0200

    Start + end whitespaces
    Allow whitespaces at text start and end

--- scripts/text-helper.js
+++ scripts/text-helper.js
@@ -6,218 +6,219 @@ function handleInput(inputField) {
     var outputField = document.getElementById("text-helper-output");
 
     inputField.style.height = "5px";
-    inputField.style.height = (inputField.scrollHeight)+"px";
+    inputField.style.height = (inputField.scrollHeight) + "px";
     outputField.style.height = "5px";
-    outputField.style.height = (outputField.scrollHeight)+"px";
+    outputField.style.height = (outputField.scrollHeight) + "px";
 
     if (inputField.value.length > 0) {
-        outputField.dataset.output="true";
-        const textFormater = new TextFormater(inputField.value, {htmlColors: true, markdownStyle: true});
+        outputField.dataset.output = "true";
+        const textFormater = new TextFormater(inputField.value, { htmlColors: true, markdownStyle: true });
+        textFormater.generateTokens();
         outputField.value = textFormater.diamondString();
     }
     else {
-        outputField.dataset.output="false";
-        outputField.value = "";
+        outputField.dataset.output = "false";
+        outputField.value = "&r";
     }
 
 }
 
+function ltrim(str) {
+    if(!str) return str;
+    return str.replace(/^\s+/g, '');
+}
+
+function rtrim(str) {
+    if(!str) return str;
+    return str.replace(/\s+$/g, '');
+}
+
+function Token(text, settings) {
+    this.text = text;
+    this.settings = {
+        color: settings.color,
+        bold: settings.bold,
+        italic: settings.italic,
+        underlined: settings.underlined,
+        strikethrough: settings.strikethrough,
+        obfuscated: settings.obfuscated,
+    };
+}
+
 class TextFormater {
-    #formated;
-    #outputText;
-    #standardSettings;
-    #outputSettings;
+    #tokenText;
+    #tokenSettings;
 
-    /**
-     * 
-     * @param {string} text 
-     * @param {object} settings 
-     */
     constructor(text, settings) {
-        this.#formated = [];
-        this.#outputText = "";
-        this.#standardSettings = {
-            color: "",
-            bold: false,
-            italic: false,
-            underline: false,
-            strikethrough: false,
-            obfuscated: false
-        };
-        this.#resetSettings();
+        this.text = text;
+        this.settings = settings;
+        this.tokens = [];
+    }
 
-        for (var i = 0; i < text.length; i++) {
-            const char = text[i];
-            if (char === "\\") {
-                if (i + 1 >= text.length){
-                    continue;
-                }
-                this.#addText(text[i + 1]);
-                i++;
+    generateTokens() {
+        var endingString = "";
+        this.#tokenText = "";
+        this.#resetTokenSettings();
+
+        for (let i = 0; i < this.text.length; i += 1) {
+            const currentChar = this.text[i];
+            if (!/\s/.test(currentChar))
+                break;
+            this.#tokenText += `&r${currentChar}`;
+        }
+        if (this.#tokenText.length > 0) {
+            this.#tokenText += "&r";
+            this.text = ltrim(this.text);
+        }
+
+        for (let i = this.text.length - 1; i >= 0; i -= 1) {
+            const currentChar = this.text[i];
+            if (!/\s/.test(currentChar))
+                break;
+            endingString += `&r${currentChar}`;
+        }
+        if (endingString.length > 0) {
+            endingString += "&r";
+            this.text = rtrim(this.text);
+        }
+        
+        for (let i = 0; i < this.text.length; i += 2) {
+            const currentChar = this.text[i];
+            var nextChar = "";
+            if (i + 1 < this.text.length) {
+                nextChar = this.text[i + 1];
+            }
+
+            if (currentChar === "\\") {
+                this.#tokenText += nextChar;
                 continue;
             }
-            if (settings.htmlColors && char === "#") {
+            if (this.settings.htmlColors && currentChar === "#") {
                 var colorCode = "&x";
-                for (let j = 1; j < 7; j++) {
-                    if (i + j >= text.length) {
+
+                for (let j = i + 1; j < this.text.length && j - i - 1 < 7; j++) {
+                    const c = this.text[j].toLowerCase();
+
+                    if (c.match(/[0-9]|[a-f]/g) == null) {
                         break;
                     }
-                    const c = text[i + j].toLowerCase();
-                    if (c.match(/[0-9]|[a-f]/g) == null){
-                        break;
-                    }
-                    colorCode += `&${c}`
+                    colorCode += `&${c}`;
                 }
+
                 if (colorCode.length >= 14) {
-                    this.#setSetting("color", colorCode.substring(0, 14));
-                    i += 6;
+                    this.#setTokenSetting("color", colorCode.substring(0, 14))
+                    i += 5;
                     continue;
                 }
                 if (colorCode.length >= 8) {
-                    this.#setSetting("color", colorCode.substring(0, 8));
-                    i += 3;
+                    this.#setTokenSetting("color", colorCode.substring(0, 8))
+                    i += 2;
                     continue;
                 }
             }
-            if (settings.markdownStyle && char === "*") {
-                if (text[i + 1] === "*") {
-                    const boldSetting = this.#outputSettings.bold;
-                    this.#setSetting("bold", !boldSetting);
-                    i++;
+            if (this.settings.markdownStyle && currentChar === "*" && nextChar === "*") {
+                this.#setTokenSetting("bold", !this.#tokenSettings.bold);
+                continue;
+            }
+            if (this.settings.markdownStyle && currentChar === "_" && nextChar === "_") {
+                this.#setTokenSetting("italic", !this.#tokenSettings.italic);
+                continue;
+            }
+            if (this.settings.markdownStyle && currentChar === "~" && nextChar === "~") {
+                this.#setTokenSetting("underline", !this.#tokenSettings.underline);
+                continue;
+            }
+            if (this.settings.markdownStyle && currentChar === "ˋ" && nextChar === "ˋ") {
+                this.#setTokenSetting("strikethrough", !this.#tokenSettings.strikethrough);
+                continue;
+            }
+            if (this.settings.markdownStyle && currentChar === "§" && nextChar === "§") {
+                this.#setTokenSetting("obfuscated", !this.#tokenSettings.obfuscated);
+                continue;
+            }
+            if (currentChar === "&") {
+                if (nextChar === "r") {
+                    this.#resetTokenSettings;
+                    continue;
+                }
+                if (nextChar.match(/[0-9]|[a-f]/g) != null) {
+                    this.#setTokenSetting("color", "&" + nextChar);
                     continue;
                 }
             }
-            if (settings.markdownStyle && char === "_") {
-                if (text[i + 1] === "_") {
-                    const italicSetting = this.#outputSettings.italic;
-                    this.#setSetting("italic", !italicSetting);
-                    i++;
-                    continue;
-                }
-            }
-            if (settings.markdownStyle && char === "~") {
-                if (text[i + 1] === "~") {
-                    const underlineSetting = this.#outputSettings.underline;
-                    this.#setSetting("underline", !underlineSetting);
-                    i++;
-                    continue;
-                }
-            }
-            if (settings.markdownStyle && char === "`") {
-                if (text[i + 1] === "`") {
-                    const strikethroughSetting = this.#outputSettings.strikethrough;
-                    this.#setSetting("strikethrough", !strikethroughSetting);
-                    i++;
-                    continue;
-                }
-            }
-            if (settings.markdownStyle && char === "§") {
-                if (text[i + 1] === "§") {
-                    const obfuscatedSetting = this.#outputSettings.obfuscated;
-                    this.#setSetting("obfuscated", !obfuscatedSetting);
-                    i++;
-                    continue;
-                }
-            }
-            if (char === "&") {
-                const nextChar = text[i + 1];
-                if (nextChar === "r"){
-                    this.#endFormat();
-                    this.#resetSettings();
-                    i++;
-                    continue;
-                }
-                if (i + 1 < text.length && nextChar.match(/[0-9]|[a-f]/g) != null){
-                    this.#setSetting("color", "&" + nextChar);
-                    i++;
-                    continue;
-                }
-            }
-            this.#addText(char);
+
+            this.#tokenText += currentChar;
+            i--;
         }
-        if (this.#outputText.length > 0) {
-            this.#endFormat();
+
+        this.#tokenText += endingString;
+        if (this.#tokenText.length > 0)
+            this.tokens.push(new Token(this.#tokenText, this.#tokenSettings));
+        return this.tokens;
+    }
+
+    #resetTokenSettings() {
+        this.#tokenSettings = {
+            color: "",
+            bold: false,
+            italic: false,
+            underlined: false,
+            strikethrough: false,
+            obfuscated: false
         }
     }
 
-    /**
-     * 
-     * @param {string} key 
-     * @param {string} value 
-     */
-    #setSetting(key, value) {
-        if (this.#outputText.length > 0)
-            this.#endFormat();
-        this.#outputSettings[key] = value;
-    }
-
-    /**
-     * 
-     * @param {string} text 
-     */
-    #addText(text) {
-        this.#outputText += text;
-    }
-
-    #endFormat() {
-        this.#formated.push({
-            text: this.#outputText,
-            settings: {
-                color: this.#outputSettings.color,
-                bold: this.#outputSettings.bold,
-                italic: this.#outputSettings.italic,
-                underline: this.#outputSettings.underline,
-                strikethrough: this.#outputSettings.strikethrough,
-                obfuscated: this.#outputSettings.obfuscated
-            }
-        });
-        this.#outputText = "";
-    }
-
-    #resetSettings() {
-        this.#outputSettings = {
-            color: this.#standardSettings.color,
-            bold: this.#standardSettings.bold,
-            italic: this.#standardSettings.italic,
-            underline: this.#standardSettings.underline,
-            strikethrough: this.#standardSettings.strikethrough,
-            obfuscated: this.#standardSettings.obfuscated
-        };
-    }
-
-    /**
-     * 
-     * @returns {object[]}
-     */
-    getFormated() {
-        return this.#formated;
+    #setTokenSetting(key, value) {
+        if (this.#tokenText.length > 0) {
+            this.tokens.push(new Token(this.#tokenText, this.#tokenSettings));
+            this.#tokenText = "";
+        }
+        this.#tokenSettings[key] = value;
     }
 
     diamondString() {
-        var text = "";
-        for (const formatedText of this.#formated) {
-            text += "&r" + formatedText.settings.color;
-            if (formatedText.settings.bold) {
-                text += "&l";
+        var dString = "";
+        var lastSettings = undefined;
+
+        for (const token of this.tokens) {
+            if (lastSettings !== undefined) {
+                for (const key of Object.keys(token.settings)) {
+                    if ((token.settings[key] === false && lastSettings[key] === true) || (token.settings.color === "" && lastSettings.color !== "")) {
+                        dString += "&r";
+                        break;
+                    }
+                }
             }
-            if (formatedText.settings.italic) {
-                text += "&o";
+
+            if (lastSettings === undefined || lastSettings.color !== token.settings.color) {
+                dString += token.settings.color;
             }
-            if (formatedText.settings.underline) {
-                text += "&n";
+
+            if (token.settings.bold) {
+                dString += "&l";
             }
-            if (formatedText.settings.strikethrough) {
-                text += "&m";
+            if (token.settings.italic) {
+                dString += "&o";
             }
-            if (formatedText.settings.obfuscated) {
-                text += "&k";
+            if (token.settings.underline) {
+                dString += "&n";
             }
-            text += formatedText.text;
-        }
-        if (text.length == 0) {
-            return "&r";
+            if (token.settings.strikethrough) {
+                dString += "&m";
+            }
+            if (token.settings.obfuscated) {
+                dString += "&k";
+            }
+
+            dString += token.text;
+
+            lastSettings = token.settings;
         }
 
-        return text;
+        if (dString === "") {
+            dString = "&r";
+        }
+
+        return dString;
     }
 }
