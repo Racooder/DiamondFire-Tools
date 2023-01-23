5cc572fe069b6301947163eb14c86bb697360104
Racooder <76227605+Racooder@users.noreply.github.com>
2023-01-23 21:53:22 +0100

    Added Command List Function

--- scripts/commands.js
+++ scripts/commands.js
@@ -1,8 +1,74 @@
 $(document).ready(function () {
-    $(".foldable").click(function () {
-        switchFold(this);
+    loadCommands();
+    $("#command-search-input").on("keypress", function (event) {
+        if (event.keyCode == 13) {
+            searchCommands();
+        }
     });
 });
+
+function loadCommands(filter) {
+    $.get(`https://raw.githubusercontent.com/Racooder/DiamondFire-Tools/2.1-Commands/data/commands.json`, function (data) {
+        const commandList = JSON.parse(data);
+        for (const cmdGroup of commandList) {
+            let foundFilter = false;
+            let groupIsFilter = false;
+            if (cmdGroup.title.toLowerCase().includes(filter)) {
+                foundFilter = true;
+                groupIsFilter = true;
+            }
+            let groupObject = `<div class="foldable unfolded"><span>${cmdGroup.title.charAt(0).toUpperCase() + cmdGroup.title.slice(1)} <img class="dropdown-arrow"></span>`;
+            for (const cmd of cmdGroup.commands) {
+                if (!groupIsFilter) {
+                    let found = false;
+                    if (cmd.description.toLowerCase().includes(filter)) {
+                        found = true;
+                        foundFilter = true;
+                    }
+                    for (const instruction of cmd.commands) {
+                        if (instruction.toLowerCase().includes(filter)) {
+                            found = true;
+                            foundFilter = true;
+                        }
+                    }
+                    if (!found && filter != undefined) {
+                        continue;
+                    }
+                }
+                groupObject += `<ul class="command-list"><li class="command-list"><h3>${cmd.commands[0].charAt(0).toUpperCase() + cmd.commands[0].slice(1)}</h3>`;
+                groupObject += `<ul>`;
+                for (const instruction of cmd.commands) {
+                    groupObject += `<li>/${instruction}`;
+                    for (const arg of cmd.args) {
+                        groupObject += ` &lt;${arg}&gt;`;
+                    }
+                    for (const opt of cmd.optionals) {
+                        groupObject += ` [${opt}]`;
+                    }
+                    groupObject += "</li>";
+                }
+                groupObject += `</ul>`;
+                groupObject += `<span>${cmd.description}</span>`;
+                groupObject += `</li>`;
+                groupObject += "</ul>";
+            }
+            groupObject += "</div>";
+            if (foundFilter || filter == undefined) {
+                $("#command-list").append(groupObject);
+            }
+        }
+    });
+    delay(500).then(() => {
+        $(".foldable").click(function () {
+            switchFold(this);
+        });
+    });
+}
+
+function searchCommands() {
+    $("#command-list").empty();
+    loadCommands($("#command-search-input").val().toLowerCase());
+}
 
 function switchFold(element) {
     if ($(element).hasClass("folded")) {
@@ -12,4 +78,8 @@ function switchFold(element) {
         $(element).removeClass("unfolded");
         $(element).addClass("folded");
     }
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
 }
